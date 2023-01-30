3012e994b6cd2f8d2a62e178a8abfbff1638a7c1
AleksandrMinyshk <alexmech001@gmail.com>
2023-01-30 13:08:37 -0500

    JavaHW 1/30/2023

---
+++ src/HomeWork/HWclass15/LargerNumber.java
@@ -0,0 +1,16 @@
+package HomeWork.HWclass15;
+
+public class LargerNumber {
+    /*  1) Create a method that will take 2 parameters
+              as a numbers and prints which number is larger.
+              */
+    int nums(int []  arr){
+        int largest=arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]>largest){
+                largest=arr[i];
+            }
+        }
+        return largest;
+    }
+}
